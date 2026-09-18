4d869268ed8e8e025a6f86d682dd6439b485b665
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-18 22:03:01 +0200

    port(hackerone): frontend/sw.js

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE='xbow-perso-v19';
+const CACHE='xbow-perso-v20';
 const PRECACHE=['/','/app.css','/app.js','/hackerone.js','/manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
