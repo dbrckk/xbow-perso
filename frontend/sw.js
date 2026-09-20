7f5d714feabff2782c7d5b98092ee1b2e5b1c1c1
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-20 16:27:39 +0200

    chore(pwa): refresh one-touch HackerOne assets

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE='xbow-perso-v37';
+const CACHE='xbow-perso-v38';
 const PRECACHE=['/','/app.css','/app.js','/hackerone.js','/manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
