7ebc5821a488517c1ed2abd900286b50baba2272
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-20 18:06:56 +0200

    chore(pwa): refresh server review profile assets

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE='xbow-perso-v40';
+const CACHE='xbow-perso-v41';
 const PRECACHE=['/','/app.css','/app.js','/hackerone.js','/manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
