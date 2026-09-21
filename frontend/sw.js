8d1b6807a2f589b89296dc0a11917a0952a8f7a4
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-21 10:55:31 +0200

    chore: bump cache for server batch resume

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE='xbow-perso-v46';
+const CACHE='xbow-perso-v47';
 const PRECACHE=['/','/app.css','/app.js','/hackerone.js','/manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
