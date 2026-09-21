bc8c47d5bb60ef65597fcbcf7ac9da39d29dc048
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-21 10:49:21 +0200

    chore: bump cache for auto queue preferences

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE='xbow-perso-v45';
+const CACHE='xbow-perso-v46';
 const PRECACHE=['/','/app.css','/app.js','/hackerone.js','/manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
