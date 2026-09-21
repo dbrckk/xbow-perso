f4c0e085cc2ddf308c6ce4b5b5d3fd54c086174d
dbrckk <131650085+dbrckk@users.noreply.github.com>
2026-09-21 12:40:57 +0200

    chore: bump cache for prelaunch server selection

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE='xbow-perso-v50';
+const CACHE='xbow-perso-v51';
 const PRECACHE=['/','/app.css','/app.js','/hackerone.js','/manifest.webmanifest'];
 
 self.addEventListener('install',event=>{
